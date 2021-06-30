3a60dd6dc9e9d1b78781ee53edaf35e0b07585e7
Volodymyr Zaets <vzaets@magento.com>
2021-06-29 23:14:15 -0500

    Update after code review

--- src/loaders/pr-and-issue.js
+++ src/loaders/pr-and-issue.js
@@ -9,9 +9,10 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import type { LoaderInterface } from '../api/loader-interface.js';
-import type { PullRequestData } from '../models/pullrequest.js';
-import type { graphql } from '@octokit/graphql';
+import type {LoaderInterface} from '../api/loader-interface.js';
+import type {PullRequestData} from '../models/pullrequest.js';
+import type {graphql} from '@octokit/graphql';
+
 const _ = require('lodash');
 const PullrequestLoader = require('./pullrequest');
 const IssueLoader = require('./issue');
@@ -20,17 +21,17 @@ const IssueLoader = require('./issue');
  * Class is responsible for loading issue and pull request data from Github
  */
 class PrAndIssue implements LoaderInterface {
-  githubGraphQlClient:graphql
+    githubGraphQlClient: graphql
 
-  /**
-   * @ram githubService
-   */
-  constructor(githubService:graphql) {
-      this.pullrequestLoader = new PullrequestLoader(githubService);
-      this.issueLoader = new IssueLoader(githubService);
-  }
+    /**
+     * @param githubService
+     */
+    constructor(githubService: graphql) {
+        this.pullrequestLoader = new PullrequestLoader(githubService);
+        this.issueLoader = new IssueLoader(githubService);
+    }
 
-  /**
+    /**
      * Load data from Github
      *
      * @param organization
@@ -40,17 +41,17 @@ class PrAndIssue implements LoaderInterface {
      * @param to
      * @return {Promise<{createdAt: Object.createdAt, contributionType: Object.contributionType, number: Object.number, author: Object.author.login, organization: string, repository: string, title: Object.title, url: Object.url, labels: Object.labels.nodes}[]>}
      */
-  async execute (
-  	organization:string,
-  	repository:string,
-  	branch,
-  	from:Date,
-  	to:Date
-  ):Promise<Array<PullRequestData>> {
-      const prs = await this.pullrequestLoader.execute(organization, repository, branch, from, to);
-      const issues = await this.issueLoader.execute(organization, repository, branch, from, to);
-      return [...prs, ...issues];
-
-  }
+    async execute(
+        organization: string,
+        repository: string,
+        branch,
+        from: Date,
+        to: Date
+    ): Promise<Array<PullRequestData>> {
+        const prs = await this.pullrequestLoader.execute(organization, repository, branch, from, to);
+        const issues = await this.issueLoader.execute(organization, repository, branch, from, to);
+        return [...prs, ...issues];
+    }
 }
+
 module.exports = PrAndIssue;
